9588313e1290037250b5122cf0808bb2a484dca0
BaileyLC <bailey.crowley@gmail.com>
2023-11-01 10:17:18 -0600

    Added an activity

--- lab_practice.R
+++ lab_practice.R
@@ -1,6 +1,6 @@
 ## Schaeffer Lab Practice
 
-# Prepare workspace ----
+# Prepare works pace ----
 
 # Set working directory
   setwd("~/Downloads")
@@ -8,4 +8,12 @@
 # Create a data frame
   df <- data.frame("A" = c(1:10),
                    "B" = c(11:20))
+  
+# Add a new column to df
+  df$C <- c(21:30)
+  
+# Check to see if it worked
+  df
+  
+# ACTIVITY: Add a new column to df
   
