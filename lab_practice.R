a38f830c58a1108ff358bb931a3f8d44b940f453
“emily-burgess” <emily@eburgi.com>
2023-11-01 10:20:59 -0600

    added color column

--- lab_practice.R
+++ lab_practice.R
@@ -8,4 +8,6 @@
 # Create a data frame
   df <- data.frame("A" = c(1:10),
                    "B" = c(11:20))
-  
+  #add color column to the dataframe 
+df$color<-c("red","red","red","blue","blue","blue",
+            "purple","purple","purple","purple")  
